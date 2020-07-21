2d850eee23f469d4bb4b15819c5b0254b21a82d0
strannik <strannik.mas@gmail.com>
2020-07-21 12:35:11 +0300

    delete core

--- src/core/store/sphere/mutations.ts
+++
@@ -1,9 +0,0 @@
-import {MutationTree} from 'vuex';
-import {SphereArr, SphereState} from './types';
-
-export const mutations: MutationTree<SphereState> = {
-    spheresLoaded(state, payload: Array<SphereArr>) {
-        state.error = false;
-        state.spheres = payload;
-    },
-};
